e3af0f24544fd48bd3f4f1b4ff3e7fc2d9d92fc9
OrtizEche <ortiz.eche@gmail.com>
2021-11-07 19:24:00 -0500

    firts commit

---
+++ src/app/destinos/destinos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DestinosRoutingModule } from './destinos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DestinosRoutingModule
+  ]
+})
+export class DestinosModule { }
